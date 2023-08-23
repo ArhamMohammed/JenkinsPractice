52925995b6f1e57086357c9bc74a890cda2932d3
Mir Mohammed Arham <send2arhamm@gmail.com>
2023-08-23 16:43:42 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test{
   public static void main(String []args){
-    System.out.println("Jenkins is a powerful tool to make the builds and to release it in the system.")
+    System.out.println("Welcome to Jenkins")
   }
 }
